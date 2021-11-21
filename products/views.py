6e26c3e8dc984092b1ea3a946f59fbb811943fd3
farah1979 <farahhisham1979@gmail.com>
2021-11-21 13:48:26 +0000

    added sort functionality and back to top page

--- products/views.py
+++ products/views.py
@@ -6,7 +6,6 @@ from django.db.models.functions import Lower
 
 # Create your views here.
 
-
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
@@ -30,7 +29,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -41,7 +40,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
